b66e4ea360ec8f0beecafd90cf9f060b0ea66996
Rosano Coutinho <pub@xeari.com>
2020-11-22 13:16:45 -0300

    Extract OLSKLocalized to OLSKInternational

--- main.svelte
+++ main.svelte
@@ -1,8 +1,6 @@
 <script>
-import OLSKInternational from 'OLSKInternational';
-const OLSKLocalized = function(translationConstant) {
-	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`)[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
-};
+import { OLSKLocalized } from 'OLSKInternational';
+
 import OLSKRootLink from 'OLSKRootLink';
 </script>
 
