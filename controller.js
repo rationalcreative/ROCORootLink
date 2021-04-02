326da508444795a66a386e083d9f03348d861caa
Rosano Coutinho <pub@xeari.com>
2021-04-02 12:32:51 -0300

    Localize pt

--- controller.js
+++ controller.js
@@ -8,7 +8,7 @@ exports.OLSKControllerRoutes = function() {
 			});
 		},
 		OLSKRouteSignature: 'ROCORootLinkEJSStubRoute',
-		OLSKRouteLanguageCodes: ['en', 'fr', 'es'],
+		OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt'],
 		OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
 	}, {
 		OLSKRoutePath: '/stub/ROCORootLinkSvelte',
@@ -17,7 +17,7 @@ exports.OLSKControllerRoutes = function() {
 			return res.render(require('path').join(__dirname, 'stub-view'));
 		},
 		OLSKRouteSignature: 'ROCORootLinkSvelteStubRoute',
-		OLSKRouteLanguageCodes: ['en', 'fr', 'es'],
+		OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt'],
 		OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
 	}];
 };
